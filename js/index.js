5f3db66bd2f19184b59a519defefbf695a57d816
Muhammadjon <mjalolov1305@gmail.com>
2022-08-25 22:19:43 +0500

    docs: added new features

--- js/index.js
+++ js/index.js
@@ -4,20 +4,38 @@ import difficulties from "../data/difficulties.js";
 console.log(difficulties);
 console.log(ancientsData);
 
-var element;
-element = document.getElementById("cardID");
-
 const btn = document.getElementById("mybtn");
-btn.textContent = "Click here";
-btn.addEventListener("click", showalert);
+btn.textContent = "Замешать колоду";
+btn.addEventListener("click", startGame);
 
-function showalert(){
-    element.textContent = "Start the game";
-    console.log("show");
+function startGame(){
+    console.log("Start the Game");
 }
 
-function whichCard(par){
-    console.log(par)
+function whichCard(par, id){
+    //console.log(par)
+    //console.log(id)
+
+    let allCards = document.querySelector(".kartalar").children;
+    for(let i = 0; i < allCards.length; i++){
+        allCards[i].style.backgroundColor = "orange";
+    }
+
+    let selectedCard = document.getElementById(id);
+    selectedCard.style.backgroundColor = "green";
+}
+
+function whichLevel(lvl, id){
+    //console.log(lvl)
+    //console.log(id)
+
+    let allCards = document.querySelector(".darajalar").children;
+    for(let i = 0; i < allCards.length; i++){
+        allCards[i].style.backgroundColor = "lightblue";
+    }
+
+    let selectedLevel = document.getElementById(id);
+    selectedLevel.style.backgroundColor = "green";
 }
 
 
@@ -39,7 +57,26 @@ for(let i = 0; i < ancientsData.length; i++){
     oneCard.style.margin = "20px 20px";
     oneCard.style.backgroundColor = "orange";
     oneCard.textContent = ancientsData[i].name;
-    oneCard.addEventListener("click", (e) => {whichCard(ancientsData[i].name)})
+    oneCard.id = "onecard"+i;
+    oneCard.addEventListener("click", (e) => {whichCard(ancientsData[i].name, oneCard.id)})
 
     cardsPlaceholder.appendChild(oneCard);
+}
+
+const levelsPlaceholder = document.getElementById("darajalar");
+
+for(let i = 0; i < difficulties.length; i++){
+    let oneLevel = document.createElement("div");
+    oneLevel.style.border = "1px solid black";
+    oneLevel.style.position = "relative";
+    oneLevel.style.height = "40px";
+    oneLevel.style.width = "90px";
+    oneLevel.style.margin = "10px 10px";
+    oneLevel.style.padding = "5px";
+    oneLevel.style.backgroundColor = "lightblue";
+    oneLevel.textContent = difficulties[i].name;
+    oneLevel.id = "onelevel"+i;
+    oneLevel.addEventListener("click", (e) => {whichLevel(difficulties[i].name, oneLevel.id)})
+
+    levelsPlaceholder.appendChild(oneLevel);
 }
